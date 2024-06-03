4f353310c43d0ce025cd5992cf5aab9a6fb6f684
denwer4k <maksroll9@gmail.com>
2024-06-04 00:31:03 +0500

    The Last Commit Stand:)
    Yeeeeeee

---
+++ Assets/Scropts/LeaveToMenu.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LeaveToMenu : MonoBehaviour
+{
+    
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+            SceneManager.LoadScene(0);
+    }
+}
